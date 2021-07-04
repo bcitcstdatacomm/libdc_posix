c32dc55334964a7459e984aac92adf00427ec5bb
darcy-bcit <D'Arcy_D_Smith@bcit.ca>
2021-07-04 10:37:59 -0700

    make sure errno_t is provided

--- src/posix_env.h
+++ src/posix_env.h
@@ -19,6 +19,10 @@
  */
 
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-macros"
+#define __STDC_WANT_LIB_EXT1__ 1
+#pragma GCC diagnostic pop
 #include <errno.h>
 #include <stdbool.h>
 #include <stddef.h>
