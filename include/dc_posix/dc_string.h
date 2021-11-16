6d9b1aa9c0ef2f7196db9fd85aadb36cccc16327
darcy-bcit <D'Arcy_D_Smith@bcit.ca>
2021-11-16 15:35:58 -0800

    finished string.h

--- include/dc_posix/dc_string.h
+++ include/dc_posix/dc_string.h
@@ -19,75 +19,46 @@
 
 #include "dc_posix_env.h"
 #include <stddef.h>
+#if __has_include(<locale.h>)
+#include <locale.h>
+#endif
+#if __has_include(<xlocale.h>)
+#include <xlocale.h>
+#endif
 
-/**
- *
- * @param env
- * @param s1
- * @param s2
- * @param n
- * @return
- */
-void *dc_memcpy(const struct dc_posix_env *env, void *restrict s1,
-                const void *restrict s2, size_t n);
-
-/**
- *
- * @param env
- * @param s
- * @param c
- * @param n
- * @return
- */
+void *dc_memccpy(const struct dc_posix_env *env, void *restrict s1, const void *restrict s2, int c, size_t n);
+void *dc_memchr(const struct dc_posix_env *env, const void *s, int c, size_t n);
+int dc_memcmp(const struct dc_posix_env *env, const void *s1, const void *s2, size_t n);
+void *dc_memcpy(const struct dc_posix_env *env, void *restrict s1, const void *restrict s2, size_t n);
+void *dc_memmove(const struct dc_posix_env *env, void *s1, const void *s2, size_t n);
 void *dc_memset(const struct dc_posix_env *env, void *s, int c, size_t n);
-
-/**
- *
- * @param env
- * @param s1
- * @param s2
- * @return
- */
-char *dc_strcpy(const struct dc_posix_env *env, char *restrict s1,
-                const char *restrict s2);
-
-/**
- *
- * @param env
- * @param s
- * @return
- */
-size_t dc_strlen(const struct dc_posix_env *env, const char *s);
-
-/**
- *
- * @param env
- * @param s1
- * @param s2
- * @return
- */
+char *dc_stpcpy(const struct dc_posix_env *env, char *restrict s1, const char *restrict s2);
+char *dc_stpncpy(const struct dc_posix_env *env, char *restrict s1, const char *restrict s2, size_t n);
+char *dc_strcat(const struct dc_posix_env *env, char *restrict s1, const char *restrict s2);
+char *dc_strchr(const struct dc_posix_env *env, const char *s, int c);
 int dc_strcmp(const struct dc_posix_env *env, const char *s1, const char *s2);
+int dc_strcoll(const struct dc_posix_env *env, const char *s1, const char *s2);
+int dc_strcoll_l(const struct dc_posix_env *env, const char *s1, const char *s2, locale_t locale);
+char *dc_strcpy(const struct dc_posix_env *env, char *restrict s1, const char *restrict s2);
+size_t dc_strcspn(const struct dc_posix_env *env, const char *s1, const char *s2);
+char *dc_strdup(const struct dc_posix_env *env, struct dc_error *err, const char *s);
+char *dc_strerror(const struct dc_posix_env *env, struct dc_error *err, int errnum);
+int dc_strerror_r(const struct dc_posix_env *env, int errnum, char *strerrbuf, size_t buflen);
+size_t dc_strlen(const struct dc_posix_env *env, const char *s);
+char *dc_strncat(const struct dc_posix_env *env, char *restrict s1, const char *restrict s2, size_t n);
+int dc_strncmp(const struct dc_posix_env *env, const char *s1, const char *s2, size_t n);
+char *dc_strncpy(const struct dc_posix_env *env, char *restrict s1, const char *restrict s2, size_t n);
+char *dc_strndup(const struct dc_posix_env *env, struct dc_error *err, const char *s, size_t n);
+size_t dc_strnlen(const struct dc_posix_env *env, const char *s, size_t maxlen);
+char *dc_strpbrk(const struct dc_posix_env *env, const char *s1, const char *s2);
+char *dc_strrchr(const struct dc_posix_env *env, const char *s, int c);
+char *dc_strsignal(const struct dc_posix_env *env, int signum);
+size_t dc_strspn(const struct dc_posix_env *env, const char *s1, const char *s2);
+char *dc_strstr(const struct dc_posix_env *env, const char *s1, const char *s2);
+char *dc_strtok(const struct dc_posix_env *env, char *restrict s, const char *restrict sep);
+char *dc_strtok_r(const struct dc_posix_env *env, char *restrict s, const char *restrict sep, char **restrict state);
+size_t dc_strxfrm(const struct dc_posix_env *env, struct dc_error *err, char *restrict s1, const char *restrict s2, size_t n);
+size_t dc_strxfrm_l(const struct dc_posix_env *env, struct dc_error *err, char *restrict s1, const char *restrict s2, size_t n, locale_t locale);
 
-/**
- *
- * @param env
- * @param s1
- * @param s2
- * @param n
- * @return
- */
-int dc_strncmp(const struct dc_posix_env *env, const char *s1, const char *s2,
-               size_t n);
-
-/**
- *
- * @param env
- * @param s
- * @param sep
- * @param state
- * @return
- */
-char *dc_strtok_r(const struct dc_posix_env *env, char *restrict s,
-                  const char *restrict sep, char **restrict state);
 
 #endif // LIBDC_POSIX_DC_STRING_H
--- src/string.c
+++ src/string.c
@@ -17,79 +17,391 @@
 #include "dc_string.h"
 #include <string.h>
 
-// https://pubs.opengroup.org/onlinepubs/9699919799/functions/memcpy.html
-void *dc_memcpy(const struct dc_posix_env *env, void * restrict s1, const void * restrict s2, size_t n)
+void *dc_memccpy(const struct dc_posix_env *env, void *restrict s1, const void *restrict s2, int c, size_t n)
 {
     void *ret_val;
 
     DC_TRACE(env);
+    errno = 0;
+    ret_val = memccpy(s1, s2, c, n);
+
+    return ret_val;
+}
+
+void *dc_memchr(const struct dc_posix_env *env, const void *s, int c, size_t n)
+{
+    void *ret_val;
+
+    DC_TRACE(env);
+    errno = 0;
+    ret_val = memchr(s, c, n);
+
+    return ret_val;
+}
+
+int dc_memcmp(const struct dc_posix_env *env, const void *s1, const void *s2, size_t n)
+{
+    int ret_val;
+
+    DC_TRACE(env);
+    errno = 0;
+    ret_val = memcmp(s1, s2, n);
+
+    return ret_val;
+}
+
+void *dc_memcpy(const struct dc_posix_env *env, void *restrict s1, const void *restrict s2, size_t n)
+{
+    void *ret_val;
+
+    DC_TRACE(env);
+    errno = 0;
     ret_val = memcpy(s1, s2, n);
 
     return ret_val;
 }
 
-// https://pubs.opengroup.org/onlinepubs/9699919799/functions/memset.html
+void *dc_memmove(const struct dc_posix_env *env, void *s1, const void *s2, size_t n)
+{
+    void *ret_val;
+
+    DC_TRACE(env);
+    errno = 0;
+    ret_val = memmove(s1, s2, n);
+
+    return ret_val;
+}
+
 void *dc_memset(const struct dc_posix_env *env, void *s, int c, size_t n)
 {
     void *ret_val;
 
     DC_TRACE(env);
+    errno = 0;
     ret_val = memset(s, c, n);
 
     return ret_val;
 }
 
-// https://pubs.opengroup.org/onlinepubs/9699919799/functions/strcpy.html
-char *dc_strcpy(const struct dc_posix_env *env, char * restrict s1, const char * restrict s2)
+char *dc_stpcpy(const struct dc_posix_env *env, char *restrict s1, const char *restrict s2)
 {
     char *ret_val;
 
     DC_TRACE(env);
-    ret_val = strcpy(s1, s2);
+    errno = 0;
+    ret_val = stpcpy(s1, s2);
 
     return ret_val;
 }
 
-// https://pubs.opengroup.org/onlinepubs/9699919799/functions/strlen.html
-size_t dc_strlen(const struct dc_posix_env *env, const char *s)
+char *dc_stpncpy(const struct dc_posix_env *env, char *restrict s1, const char *restrict s2, size_t n)
 {
-    size_t ret_val;
+    char *ret_val;
 
     DC_TRACE(env);
-    ret_val = strlen(s);
+    errno = 0;
+    ret_val = stpncpy(s1, s2, n);
 
     return ret_val;
 }
 
-// https://pubs.opengroup.org/onlinepubs/9699919799/functions/strcmp.html
+char *dc_strcat(const struct dc_posix_env *env, char *restrict s1, const char *restrict s2)
+{
+    char *ret_val;
+
+    DC_TRACE(env);
+    errno = 0;
+    ret_val = strcat(s1, s2);
+
+    return ret_val;
+}
+
+char *dc_strchr(const struct dc_posix_env *env, const char *s, int c)
+{
+    char *ret_val;
+
+    DC_TRACE(env);
+    errno = 0;
+    ret_val = strchr(s, c);
+
+    return ret_val;
+}
+
 int dc_strcmp(const struct dc_posix_env *env, const char *s1, const char *s2)
 {
     int ret_val;
 
     DC_TRACE(env);
+    errno = 0;
     ret_val = strcmp(s1, s2);
 
     return ret_val;
 }
 
-// https://pubs.opengroup.org/onlinepubs/9699919799/functions/strncmp.html
+int dc_strcoll(const struct dc_posix_env *env, const char *s1, const char *s2)
+{
+    int ret_val;
+
+    DC_TRACE(env);
+    errno = 0;
+    ret_val = strcoll(s1, s2);
+
+    return ret_val;
+}
+
+int dc_strcoll_l(const struct dc_posix_env *env, const char *s1, const char *s2, locale_t locale)
+{
+    int ret_val;
+
+    DC_TRACE(env);
+    errno = 0;
+    ret_val = strcoll_l(s1, s2, locale);
+
+    return ret_val;
+}
+
+char *dc_strcpy(const struct dc_posix_env *env, char *restrict s1, const char *restrict s2)
+{
+    void *ret_val;
+
+    DC_TRACE(env);
+    errno = 0;
+    ret_val = strcpy(s1, s2);
+
+    return ret_val;
+}
+
+size_t dc_strcspn(const struct dc_posix_env *env, const char *s1, const char *s2)
+{
+    size_t ret_val;
+
+    DC_TRACE(env);
+    errno = 0;
+    ret_val = strcspn(s1, s2);
+
+    return ret_val;
+}
+
+char *dc_strdup(const struct dc_posix_env *env, struct dc_error *err, const char *s)
+{
+    char *ret_val;
+
+    DC_TRACE(env);
+    errno = 0;
+    ret_val = strdup(s);
+
+    if(ret_val == NULL)
+    {
+        DC_ERROR_RAISE_ERRNO(err, errno);
+    }
+
+    return ret_val;
+}
+
+char *dc_strerror(const struct dc_posix_env *env, struct dc_error *err, int errnum)
+{
+    char *ret_val;
+
+    DC_TRACE(env);
+    errno = 0;
+    ret_val = strerror(errnum);
+
+    if(errno != 0)
+    {
+        DC_ERROR_RAISE_ERRNO(err, errno);
+    }
+
+    return ret_val;
+}
+
+int dc_strerror_r(const struct dc_posix_env *env, int errnum, char *strerrbuf, size_t buflen)
+{
+    int ret_val;
+
+    DC_TRACE(env);
+    errno = 0;
+    ret_val = strerror_r(errnum, strerrbuf, buflen);
+
+    return ret_val;
+}
+
+size_t dc_strlen(const struct dc_posix_env *env, const char *s)
+{
+    size_t ret_val;
+
+    DC_TRACE(env);
+    errno = 0;
+    ret_val = strlen(s);
+
+    return ret_val;
+}
+
+char *dc_strncat(const struct dc_posix_env *env, char *restrict s1, const char *restrict s2, size_t n)
+{
+    char *ret_val;
+
+    DC_TRACE(env);
+    errno = 0;
+    ret_val = strncat(s1, s2, n);
+
+    return ret_val;
+}
+
 int dc_strncmp(const struct dc_posix_env *env, const char *s1, const char *s2, size_t n)
 {
     int ret_val;
 
     DC_TRACE(env);
+    errno = 0;
     ret_val = strncmp(s1, s2, n);
 
     return ret_val;
 }
 
-// https://pubs.opengroup.org/onlinepubs/9699919799/functions/strtok.html
-char *dc_strtok_r(const struct dc_posix_env *env, char * restrict s, const char * restrict sep, char ** restrict state)
+char *dc_strncpy(const struct dc_posix_env *env, char *restrict s1, const char *restrict s2, size_t n)
 {
     char *ret_val;
 
     DC_TRACE(env);
+    errno = 0;
+    ret_val = strncpy(s1, s2, n);
+
+    return ret_val;
+}
+
+char *dc_strndup(const struct dc_posix_env *env, struct dc_error *err, const char *s, size_t n)
+{
+    char *ret_val;
+
+    DC_TRACE(env);
+    errno = 0;
+    ret_val = strndup(s, n);
+
+    if(ret_val == NULL)
+    {
+        DC_ERROR_RAISE_ERRNO(err, errno);
+    }
+
+    return ret_val;
+}
+
+size_t dc_strnlen(const struct dc_posix_env *env, const char *s, size_t maxlen)
+{
+    size_t ret_val;
+
+    DC_TRACE(env);
+    errno = 0;
+    ret_val = strnlen(s, maxlen);
+
+    return ret_val;
+}
+
+char *dc_strpbrk(const struct dc_posix_env *env, const char *s1, const char *s2)
+{
+    char *ret_val;
+
+    DC_TRACE(env);
+    errno = 0;
+    ret_val = strpbrk(s1, s2);
+
+    return ret_val;
+}
+
+
+char *dc_strrchr(const struct dc_posix_env *env, const char *s, int c)
+{
+    char *ret_val;
+
+    DC_TRACE(env);
+    errno = 0;
+    ret_val = strrchr(s, c);
+
+    return ret_val;
+}
+
+char *dc_strsignal(const struct dc_posix_env *env, int signum)
+{
+    char *ret_val;
+
+    DC_TRACE(env);
+    errno = 0;
+    ret_val = strsignal(signum);
+
+    return ret_val;
+}
+
+size_t dc_strspn(const struct dc_posix_env *env, const char *s1, const char *s2)
+{
+    size_t ret_val;
+
+    DC_TRACE(env);
+    errno = 0;
+    ret_val = strspn(s1, s2);
+
+    return ret_val;
+}
+
+char *dc_strstr(const struct dc_posix_env *env, const char *s1, const char *s2)
+{
+    char *ret_val;
+
+    DC_TRACE(env);
+    errno = 0;
+    ret_val = strstr(s1, s2);
+
+    return ret_val;
+}
+
+char *dc_strtok(const struct dc_posix_env *env, char *restrict s, const char *restrict sep)
+{
+    char *ret_val;
+
+    DC_TRACE(env);
+    errno = 0;
+    ret_val = strtok(s, sep);
+
+    return ret_val;
+}
+
+char *dc_strtok_r(const struct dc_posix_env *env, char *restrict s, const char *restrict sep, char **restrict state)
+{
+    char *ret_val;
+
+    DC_TRACE(env);
+    errno = 0;
     ret_val = strtok_r(s, sep, state);
+
+    return ret_val;
+}
+
+size_t dc_strxfrm(const struct dc_posix_env *env, struct dc_error *err, char *restrict s1, const char *restrict s2, size_t n)
+{
+    size_t ret_val;
+
+    DC_TRACE(env);
+    errno = 0;
+    ret_val = strxfrm(s1, s2, n);
+
+    if(errno != 0)
+    {
+        DC_ERROR_RAISE_ERRNO(err, errno);
+    }
+
+    return ret_val;
+}
+
+size_t dc_strxfrm_l(const struct dc_posix_env *env, struct dc_error *err, char *restrict s1, const char *restrict s2, size_t n, locale_t locale)
+{
+    size_t ret_val;
+
+    DC_TRACE(env);
+    errno = 0;
+    ret_val = strxfrm_l(s1, s2, n, locale);
+
+    if(errno != 0)
+    {
+        DC_ERROR_RAISE_ERRNO(err, errno);
+    }
 
     return ret_val;
 }
