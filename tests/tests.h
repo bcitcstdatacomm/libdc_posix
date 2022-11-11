06ebf304eee9adf3c184b700e5a500c9c1791a95
programming101dev <113087924+programming101dev@users.noreply.github.com>
2022-11-10 21:54:01 -0800

    code cleanup & setup testing

--- tests/tests.h
+++ tests/tests.h
@@ -5,45 +5,4 @@
 #include <cgreen/cgreen.h>
 
 
-TestSuite *dc_arpa_inet_tests(void);
-TestSuite *dc_net_if_tests(void);
-TestSuite *dc_sys_mman_tests(void);
-TestSuite *dc_sys_select_tests(void);
-TestSuite *dc_sys_socket_tests(void);
-TestSuite *dc_sys_stat_tests(void);
-TestSuite *dc_sys_statvfs_tests(void);
-TestSuite *dc_sys_times_tests(void);
-TestSuite *dc_sys_utsname_tests(void);
-TestSuite *dc_sys_wait_tests(void);
-TestSuite *dc_aio_tests(void);
-TestSuite *dc_dirent_tests(void);
-TestSuite *dc_dlfcn_tests(void);
-TestSuite *dc_fcntl_tests(void);
-TestSuite *dc_fnmatch_tests(void);
-TestSuite *dc_glob_tests(void);
-TestSuite *dc_grp_tests(void);
-TestSuite *dc_iconv_tests(void);
-TestSuite *dc_inttypes_tests(void);
-TestSuite *dc_langinfo_tests(void);
-TestSuite *dc_ndbm_tests(void);
-TestSuite *dc_netdb_tests(void);
-TestSuite *dc_nl_types_tests(void);
-TestSuite *dc_poll_tests(void);
-TestSuite *dc_env_tests(void);
-TestSuite *dc_pthread_tests(void);
-TestSuite *dc_pwd_tests(void);
-TestSuite *dc_regex_tests(void);
-TestSuite *dc_sched_tests(void);
-TestSuite *dc_semaphore_tests(void);
-TestSuite *dc_signal_tests(void);
-TestSuite *dc_stdio_tests(void);
-TestSuite *dc_stdlib_tests(void);
-TestSuite *dc_string_tests(void);
-TestSuite *dc_strings_tests(void);
-TestSuite *dc_termios_tests(void);
-TestSuite *dc_time_tests(void);
-TestSuite *dc_unistd_tests(void);
-TestSuite *dc_wordexp_tests(void);
-
-
 #endif // LIBDC_POSIX_TESTS_H
